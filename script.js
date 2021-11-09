a6fb3affead7da582a54cc6fda8894b2839d4ad0
Lucas Henrique Coelho <lucas.h.coelho20@gmail.com>
2021-11-09 08:34:10 -0300

    Refeito o contador com js separado

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnIncrementar$ = document.getElementById("btn_incrementar");
+const btnDecrementar$ = document.getElementById("btn_decrementar");
+const btnReset$ = document.getElementById("btn_reset");
+const p$ = document.getElementById("contador");
+
+    let contador = 0;
+     
+    p$.innerHTML = contador;
+
+    btnIncrementar$.addEventListener("click", function(){
+        contador;
+
+        p$.innerHTML = ++contador;
+    });
+
+    btnDecrementar$.addEventListener("click", function(){
+        contador;
+
+        p$.innerHTML = --contador;
+    });
+
+    btnReset$.addEventListener("click", function(){
+        contador;
+
+        p$.innerHTML = contador = 0;
+    });
